0a697015e60f140a3aab915a6be88fa6d0af3008
kleitich <kevleitich@gmail.com>
2022-05-18 07:53:53 +0300

    addding log in feature

---
+++ config.py
@@ -0,0 +1,31 @@
+
+import os 
+
+class Config:
+    ECRET_KEY = 'voke'
+    SQLALCHEMY_TRACK_MODIFICATIONS= True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1421@localhost/petstore'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+     # api config
+ 
+
+
+
+class ProdConfig(Config):
+    pass
+ 
+class DevConfig(Config):
+    DEBUG = True
+
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
--- env/bin/activate.csh
+++ env/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/joy/Documents/Moringa-Core/week5/Petstore/env"
+setenv VIRTUAL_ENV "/home/moringa/Desktop/Petstore/env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
